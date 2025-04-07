28da2b0512b96d626c0a4e9d79ce59b6212d0474
WebAmadeus <amadeus@kubiak.se>
2025-04-07 12:33:54 +0200

    Create CNCA.rb

---
+++ CNCA.rb
@@ -0,0 +1,9 @@
+def CNCA()
+  puts "Welcome to Cinco Noches con Alfredo!"
+  puts "Choose your character:"
+  puts "Carlos or Valentina"
+  if gets.chomp == "Carlos"
+  elsif gets.chomp == "Valentina"
+
+  puts "It was a late December night. You were sitting alone at the bar, nursing a drink, when a plump, comforting man approached. Drunk and in need of solace, you agreed to follow him. During your conversation, he introduced himself as Alfredo—the owner of Hotel de Español, a nearby establishment. With a storm raging outside, Alfredo kindly offers shelter instead of letting you face the cold, wet night. You accept and follow him to his hotel."
+  
