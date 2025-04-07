9fd249f030cddaf89f2878defc08380294156e34
WebAmadeus <amadeus@kubiak.se>
2025-04-07 12:42:11 +0200

    Jag kopierade in vårt manus in i en txt fil nu så vi kan referera till den ist för att kopiera in allt i koden

--- CNCA.rb
+++ CNCA.rb
@@ -4,6 +4,9 @@ def CNCA()
   puts "Carlos or Valentina"
   if gets.chomp == "Carlos"
   elsif gets.chomp == "Valentina"
-
+  end 
   puts "It was a late December night. You were sitting alone at the bar, nursing a drink, when a plump, comforting man approached. Drunk and in need of solace, you agreed to follow him. During your conversation, he introduced himself as Alfredo—the owner of Hotel de Español, a nearby establishment. With a storm raging outside, Alfredo kindly offers shelter instead of letting you face the cold, wet night. You accept and follow him to his hotel."
   
+end 
+
+CNCA()
