34e468e0010bb4edd9320195547b74b29e9a0b0b
Wiktor <wiktor.nordstrm@elev.ga.ntig.se>
2025-04-08 12:59:00 +0200

    nya functioner scriptcall och choice för att förenkla kod strukturen

--- CNCA.rb
+++ CNCA.rb
@@ -1,29 +1,34 @@
 def CNCA()
   lines = File.readlines("totalscript.txt")
 
-  start_line = 2
-  end_line = 4
+
+  puts Scriptcall(2,4)
+  Choice("Carlos", "Valentina") #Tyckte det kändes kul att an kan välja karaktär och att det sedan inte gör någon skillnad:)
+  puts Scriptcall(7,8)
+  puts Scriptcall(11, 17)
+  Choice("Wait","Explore")
+end 
+
+
+
+
+def Scriptcall (start_line, end_line)
+lines = File.readlines("totalscript.txt")
 
   # jag subtraherar  1 från "star_line" för att få mer användarvänlig kod. Alltså så att index skall stämma överens med siffran i min totalscript txt
   paragraph = lines[(start_line - 1)..(end_line - 1)]
   #.join konkatinerar strängarna i paragraph
-  puts paragraph.join
+  return paragraph.join
 
-  if gets.chomp == "Carlos"
-  elsif gets.chomp == "Valentina"
-  end #Tyckte det kändes kul att an kan välja karaktär och att det sedan inte gör någon skillnad:)
-  
-  line_number = 7  #läser upp rad 7 i totalscript.txt
-  puts lines[line_number - 1]
-  
-  start_line = 11
-  end_line = 17
+end
 
-  paragraph = lines[(start_line - 1)..(end_line - 1)]
-  puts paragraph.join
+def Choice(choice1,choice2)
 
-  if gets.chomp == wait
-    mamma mia
-end 
+  if gets.chomp.to_s == choice1
+      
+  elsif gets.chomp.to_s == choice2
+      
+  end
+end
 
 CNCA()
